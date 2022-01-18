c79b9176d9a933ae3a3f7e9b1ef307ca0fd312e8
Muginga <74222775+muginga72@users.noreply.github.com>
2022-01-17 23:26:46 -0500

    Fix image issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Contact from './components/Contact';
+// import whiskerson from './components/img/whiskerson.jpg'
+// import fluffykins from './components/img/fluffykins.jpg'
+// import felix from './components/img/felix.jpg'
+// import pumpkin from './components/img/pumpkin.jpg'
+import './styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contacts">
+      <Contact 
+        img="whiskerson.jpg" alt=""
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact 
+        img="fluffykins.jpg" alt=""
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <Contact 
+        img="felix.jpg" alt=""
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+      />
+      <Contact 
+        img="pumpkin.jpg" alt=""
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="pumpkin@scrimba.com"
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
